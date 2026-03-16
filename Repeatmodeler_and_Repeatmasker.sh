61bf0eff7a57f99f5dece797152b3f6a5024e942
Sameer Pokhrel <pokhrelsameer60@gmail.com>
2026-03-16 18:41:50 -0400

    Update module loading in Repeatmodeler_and_Repeatmasker.sh
    Removed specific module versions for RepeatModeler, RepeatMasker, and BioPerl to load the latest versions.

--- Repeatmodeler_and_Repeatmasker.sh
+++ Repeatmodeler_and_Repeatmasker.sh
@@ -1,16 +1,8 @@
-sp47126@ss-sub1 FINAL_RESULTS_Only$ more  Repeatmodeler_repeatmasker.sh
-#!/bin/bash
-#SBATCH --job-name=repeat_mmaydis
-#SBATCH --partition=batch
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=64
-#SBATCH --mem=80gb
-#SBATCH --time=8:00:00
-#SBATCH --output=log.repeat.%j
 
-module load RepeatModeler/2.0.4-foss-2022a
-module load RepeatMasker/4.2.3-foss-2023a
-module load BioPerl/1.7.8-GCCcore-14.2.0
+
+module load RepeatModeler
+module load RepeatMasker
+module load BioPerl
 
 GENOME="Mmaydis_genome_filtered_contig1to88.fasta"
 
